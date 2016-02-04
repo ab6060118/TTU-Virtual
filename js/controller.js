ef0c914ffbaf333e472ac24ce237d521941943be
dauba <dauba.k@inwinstack.com>
2016-02-04 18:27:07 +0800

    [update] vm list update

--- js/controller.js
+++ js/controller.js
@@ -3,7 +3,10 @@
 angular.module('controllers', [])
 .controller('DashboardCtrl', ['$scope', function($scope) {
 }])
-.controller('VMListCtrl', ['$scope', function($scope, VM) {
+.controller('VMListCtrl', ['$scope', function($scope, $route, VM) {
+    $scope.$on('$routeChangeSuccess', function() {
+        $('.ui.dropdown').dropdown();
+    });
 }])
 .controller('ImageListCtrl', ['$scope', function($scope) {
 }]);
