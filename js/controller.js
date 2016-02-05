03ca405a472673ea57477decb7d2535a8ea40a0e
dauba <dauba.k@inwinstack.com>
2016-02-05 16:14:21 +0800

    [update]

--- js/controller.js
+++ js/controller.js
@@ -4,9 +4,15 @@ angular.module('controllers', [])
 .controller('DashboardCtrl', ['$scope', function($scope) {
 }])
 .controller('VMListCtrl', ['$scope', function($scope, $route, VM) {
+    $scope.templates = [{"name" : "vm-create-form", "url" : "view/vm-create-form.html"}];
+
+    $scope.template = $scope.templates[0];
+    console.log($scope.template);
+
     $scope.$on('$routeChangeSuccess', function() {
         $('.ui.dropdown').dropdown();
     });
+
 }])
 .controller('ImageListCtrl', ['$scope', function($scope) {
 }]);
