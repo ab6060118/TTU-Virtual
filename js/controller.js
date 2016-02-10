78ddc9030f4125f235938bfd110adef094c99b10
Dabua <ab6060118@gmail.com>
2016-02-10 23:10:13 +0800

    [update]

--- js/controller.js
+++ js/controller.js
@@ -3,7 +3,12 @@
 angular.module('controllers', [])
 .controller('DashboardCtrl', ['$scope', function($scope) {
 }])
-.controller('VMListCtrl', ['$scope', function($scope, $route, VM) {
+.controller('VMListCtrl', ['$scope', '$route', '$interval', 'VM', function($scope, $route, $interval, VM) {
+    $scope.VM = VM;
+    $scope.VM.stateReset();
+    
+    $scope.VM.getList('vboxGetMachines', null, null);
+    
     $scope.state = {
         creating: false,
     };
@@ -12,13 +17,56 @@ angular.module('controllers', [])
 
     $scope.template = $scope.templates[0];
 
-    $scope.$on('$routeChangeSuccess', function() {
+    $scope.doSomething = function() {
         $('.ui.dropdown').dropdown();
-    });
+    };
+    
+    $scope.powerUp = function(id) {
+        $scope.VM.data.VMs[id].state = 'Starting';
+        $scope.VM.powerUp('machineSetState', {"vm":id, "state":"powerUp"}, null)
+        .then(function(response) {
+            response = response.data.data;
+            var timer = $interval(function() {
+                $scope.VM.progressGet('progressGet', response.responseData, response.persist)
+                .then(function(response) {
+                    if(response.data.data.responseData.info.completed) {
+                        $scope.VM.data.VMs[id].state = 'Running';
+                        $interval.cancel(timer);
+                    }
+                });
+            }, 1500);
+        });
+    };
+
+    $scope.powerDown = function(id) {
+        $scope.VM.data.VMs[id].state = 'Closing';
+        $scope.VM.powerDown('machineSetState', {"vm":id, "state":"powerDown"}, null)
+        .then(function(response) {
+            response = response.data.data;
+            var timer = $interval(function() {
+                $scope.VM.progressGet('progressGet', response.responseData, response.persist)
+                .then(function(response) {
+                    if(response.data.data.responseData.info.completed) {
+                        $scope.VM.data.VMs[id].state = 'PoweredOff';
+                        $interval.cancel(timer);
+                    }
+                });
+            }, 1500);
+        });
+    };
 
     $scope.$on('cancelCreating', function() {
         $scope.state.creating = false;
     });
+    
+    $scope.reset = function() {
+    };
+    
+    $scope.init = function() {
+        $scope.reset();
+    };
+    
+    $scope.init();
 }])
 .controller('VMCreateFormCtrl', ['$scope', function($scope) {
     $scope.cancel = function(){
--- js/services.js
+++ js/services.js
@@ -1,5 +1,115 @@
 'use strict';
 
 angular.module('services', [])
-.factory('VM', ['$http', function($http) {
+.constant('API_Endpoint', '/virtualbox/endpoints/api.php')
+.factory('VM', ['$http', 'API_Endpoint', function($http, API) {
+    var self = this;
+    
+    this.data = {
+        VMs: {},
+    };
+    
+    this.state = {
+        List: {
+            loading: false,
+            loaded: false,
+        },
+        Detail: {
+            loading: false,
+            loaded: false,
+        }
+    };
+    
+    this.getList = function(fn, params, persist) {
+        self.state.List.loading = true;
+        
+        return $http({
+            method: 'POST',
+            url: API,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            data: $.param({
+                fn: fn,
+                params: params,
+                persist: persist
+            }),
+        })
+        .then(function(response) {
+            angular.forEach(response.data.data.responseData, function(value, key) {
+                self.getDetail("machineGetDetails", {"vm" : value.id}, null, value.state);
+            });
+        })
+        .finally(function() {
+            self.state.List.loaded = true;
+            self.state.List.loading = false;
+        });
+    };
+    
+    this.getDetail = function(fn, params, persist, state) {
+        return $http({
+            method: 'POST',
+            url: API,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            data: $.param({
+                fn : fn,
+                params : params,
+                persist: persist
+            }),
+        })
+        .then(function(response) {
+            response.data.data.responseData.state = state;
+            self.data.VMs[response.data.data.responseData.id] = response.data.data.responseData;
+        });
+    };
+    
+    this.powerUp = function(fn, params, persist) {
+        return $http({
+            method: 'POST',
+            url: API,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            data: $.param({
+                fn : fn,
+                params : params,
+                persist : persist
+            }),
+        });
+    };
+    
+    this.powerDown = function(fn, params, persist) {
+        return $http({
+            method: 'POST',
+            url: API,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            data: $.param({
+                fn : fn,
+                params : params,
+                persist : persist
+            }),
+        });
+    };
+    
+    this.progressGet = function(fn, params, persist) {
+        return $http({
+            method: 'POST',
+            url: API,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            data: $.param({
+                fn: fn,
+                params: params,
+                persist: persist
+            })
+        });
+    };
+    
+    this.getObjLength = function(obj) {
+        return Object.keys(obj).length;
+    }
+    
+    this.stateReset = function() {
+        self.state.List.loading = false;
+        self.state.List.loaded = false;
+        
+        self.data.VMs = {};
+    };
+    
+    return this;
 }]);
